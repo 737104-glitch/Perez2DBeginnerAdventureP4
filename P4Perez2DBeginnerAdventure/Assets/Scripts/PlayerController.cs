52526b1bfedc98aa1aafdc2d00a52cceea9f623e
737104-glitch <737104@student.dusd.net>
2025-12-02 11:44:29 -0800

    Damage
    Worked on  the player taking damage

--- P4Perez2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P4Perez2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -10,9 +10,14 @@ public class PlayerController : MonoBehaviour
     Vector2 move;
     public float speed = 3.0f;
 
-    public int health { get { return currentHealth; }}
+    public int health { get { return currentHealth; } }
     public int maxHealth = 5;
     int currentHealth;
+
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float damageCooldown;
 
     // Start is called before the first frame update
     void Start()
@@ -26,8 +31,22 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
-        
+
+
+
+        if (isInvincible)
+        {
+            damageCooldown -= Time.deltaTime;
+            if (damageCooldown < 0)
+            {
+                isInvincible = false;
+            }
+        }
     }
+
+
+
+
 
     void FixedUpdate()
     {
@@ -35,8 +54,20 @@ public class PlayerController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-    public void ChangeHealth (int amount)
-    {
+    public void ChangeHealth(int amount)
+    {   
+        if (amount  < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            damageCooldown = timeInvincible;
+        
+    }
+
+    
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
